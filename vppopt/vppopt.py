4c06a2ee83a2bd76303effd098f99016b009b4d0
longlevan <long.le-van@outlook.com>
2021-07-27 10:17:19 +0200

    update arguments for run_vppopt

--- vppopt/vppopt.py
+++ vppopt/vppopt.py
@@ -1,29 +1,29 @@
 
 import logging
-from sys import executable
 from oemof.tools import logger
 from oemof import solph
 import pandas as pd
 import datetime
 
-def run_vppopt(model, senario,**kwargs):
+def run_vppopt(nodes,**kwargs):
     """
     :param model - pyomo abstract model
     """
     timeindex = kwargs.get('timeindex',None)
-    periods = kwargs.get('periods',24)
+    periods = kwargs.get('periods',8760)
 
     solver = kwargs.get('solver','cbc')
-    solver_io = kwargs.get('solver_io','lp'),
+    solver_io = kwargs.get('solver_io',"lp")
     executable = kwargs.get('executable',"")
     solve_kwargs = kwargs.get('solve_kwargs',{})
-
-    if not timeindex:
-        start_date = datetime.datetime(1,1,datetime.datetime.now().year)
-        timeindex = pd.date_range(start=start_date,periods=periods,freq='H')
+    solver_cmd_options = kwargs.get("cmdline_options",{})
 
     logger.define_logging()
-    
+
+    if not timeindex:
+        logging.info("No timeindex is specified, only 24 hours will be considered as simulation time")
+        start_date = datetime.datetime(datetime.datetime.now().year,1,1)
+        timeindex = pd.date_range(start=start_date,periods=periods,freq='H')    
 
     # model creation and solving
     logging.info("Starting optimization")
@@ -31,12 +31,31 @@ def run_vppopt(model, senario,**kwargs):
     # initilisation of the energy system
     energy_system = solph.EnergySystem(timeindex=timeindex)
 
+    # add nodes and flows to energy system
+    energy_system.add(*nodes)
+
+    print("*********************************************************")
+    print("The following objects have been added to energy system object:")
+    for n in energy_system.nodes:
+        oobj = str(type(n)).replace("<class 'oemof.solph.", "").replace("'>", "")
+        print(oobj + ":", n.label)
+    print("*********************************************************")
+
     #####################################
     # Optimize the energy system
     #####################################
     om = solph.Model(energy_system)
 
-    # solving the linear problem using the given solver
-    om.solve(solver=solver,solver_io=solver_io,executable=executable,solve_kwargs=solve_kwargs)
+    # TODO: Do not understand what is it for?
+    om.receive_duals()
 
-    return
+    # solving the linear problem using the given solver
+    om.solve(
+        solver=solver,
+        solver_io=solver_io,
+        executable=executable,
+        solve_kwargs=solve_kwargs,
+        cmdline_options = solver_cmd_options        
+        )
+
+    return energy_system, om
